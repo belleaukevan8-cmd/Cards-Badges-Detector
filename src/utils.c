3820b2a6b8e2714ab0fc6bfaa3e854ffb2e768f6
Kw0e <belleaukevan8@gmail.com>
2026-02-22 19:56:53 +0100

    Create utility functions in src/utils.c

---
+++ src/utils.c
@@ -0,0 +1,13 @@
+// Utility functions implementation
+
+#include <stdio.h>
+
+// Function to print a greeting message
+void printGreeting() {
+    printf("Hello, welcome to the Cards-Badges-Detector!\n");
+}
+
+// Function to calculate the total number of badges
+int totalBadges(int badgeCount1, int badgeCount2) {
+    return badgeCount1 + badgeCount2;
+}
